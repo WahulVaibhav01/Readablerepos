4f7b4575a721c6789a53b83d7739f00a99cbfdef
vaibhav <wahulvaibhav@email.com>
2023-02-04 15:50:43 -0500

    Initail commit

---
+++ jmp/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int no1,no2,Add;
+clrscr();
+printf("Enter two values");
+scanf("%d%d",&no1,&no2);
+Add=no1+no2;
+printf("Ans is %d",Add);
+getch();
+}
